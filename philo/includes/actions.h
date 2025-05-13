2eda48daab2dfe6efec62b32389dbf8668300599
nseon <nseon@student.42lyon.fr>
2025-05-13 11:13:43 +0200

    finish ? deadock maybe ?

--- philo/includes/actions.h
+++ philo/includes/actions.h
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:13:11 by nseon             #+#    #+#             */
-/*   Updated: 2025/05/12 18:34:24 by nseon            ###   ########.fr       */
+/*   Updated: 2025/05/13 10:27:37 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 int		take_fork(t_args infos);
 int		is_eating(t_args *infos);
-int		philo_sleep(int	time_left, int sleep_time);
+int		philo_sleep(t_args infos, int sleep_time);
 void	is_dead(t_args infos);
 int		is_sleeping(t_args *infos);
 void	check_death(t_args infos);
--- philo/src/actions/sleep.c
+++ philo/src/actions/sleep.c
@@ -6,7 +6,7 @@
 /*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/30 14:51:03 by nseon             #+#    #+#             */
-/*   Updated: 2025/05/12 19:06:15 by nseon            ###   ########.fr       */
+/*   Updated: 2025/05/13 10:33:50 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 #include <stdio.h>
 #include "actions.h"
 
-int	philo_sleep(int	time_left, int sleep_time)
+int	philo_sleep(t_args infos, int sleep_time)
 {
-	int	count;
+	long int	awake;
 
-	count = -1;
-	while ((++count) < sleep_time)
+	awake = get_time(*infos.time_minus) + sleep_time;
+	while (get_time(*infos.time_minus) <= awake)
 	{
 		usleep(1000);
-		if (count > time_left)
+		if (get_time(*infos.time_minus) > infos.will_die)
 			return (1);
 	}
 	return (0);
@@ -32,7 +32,7 @@ int	philo_sleep(int	time_left, int sleep_time)
 int	is_sleeping(t_args *infos)
 {
 	print(*infos, "%ld %d is sleeping\n");
-	if (philo_sleep(infos->will_die - get_time(*infos->time_minus), (*infos).tt_sleep))
+	if (philo_sleep(*infos, (*infos).tt_sleep))
 	{
 		is_dead(*infos);
 		return (0);
